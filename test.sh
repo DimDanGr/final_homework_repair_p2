6343a69b4129026c40a467a795be8f00e17d42c3
Dima <dmitrygranovsky@gmail.com>
2025-07-07 10:34:33 +0300

    Created test.sh and (1,2).txt files

---
+++ test.sh
@@ -0,0 +1,14 @@
+echo
+echo "contents of file 1.txt: "
+cat 1.txt
+
+echo "after runnig it, you'll get:"
+bash 1.txt
+echo
+echo ------------------------------
+echo
+echo "contents of file 2.txt: "
+cat 2.txt
+echo
+echo "after runnig it, you'll get:"
+bash 2.txt
